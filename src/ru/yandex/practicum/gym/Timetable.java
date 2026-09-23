3a73cb8fdad1c1a89576a19020a0868bdce34b66
gorushkin <gorushkin@users.noreply.github.com>
2026-09-23 16:07:27 +0300

    feat: implement Timetable
    implement addNewTrainingSession, getTrainingSessionsForDay, getTrainingSessionsForDayAndTime

--- src/ru/yandex/practicum/gym/Timetable.java
+++ src/ru/yandex/practicum/gym/Timetable.java
@@ -4,17 +4,47 @@ import java.util.*;
 
 public class Timetable {
 
-    private /* как это хранить??? */ timetable;
+    private final Map<DayOfWeek, TreeMap<TimeOfDay, List<TrainingSession>>> timetable = new HashMap<>();
+
+    private TreeMap<TimeOfDay, List<TrainingSession>> getDayOfWeekSessions(TrainingSession trainingSession) {
+        var dayOfWeek = trainingSession.getDayOfWeek();
+
+        var dayOfWeekSessions = timetable.getOrDefault(dayOfWeek, null);
+
+        if (dayOfWeekSessions == null) {
+            timetable.put(dayOfWeek, new TreeMap<>());
+        }
+
+        return timetable.get((dayOfWeek));
+    }
+
+    private List<TrainingSession> getTimeOfDaySessions(TrainingSession trainingSession) {
+        var timeOfDay = trainingSession.getTimeOfDay();
+
+        var dayOfWeekSessions = getDayOfWeekSessions(trainingSession);
+
+        var timeOfDaySessions = dayOfWeekSessions.getOrDefault(timeOfDay, null);
+
+        if (timeOfDaySessions == null) {
+            dayOfWeekSessions.put(timeOfDay, new ArrayList<>());
+        }
+
+        return dayOfWeekSessions.get(timeOfDay);
+    }
 
     public void addNewTrainingSession(TrainingSession trainingSession) {
-        //сохраняем занятие в расписании
+
+        var timeOfDaySessions = getTimeOfDaySessions(trainingSession);
+        timeOfDaySessions.add(trainingSession);
     }
 
-    public /* непонятно, что возвращать */ getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
-        //как реализовать, тоже непонятно, но сложность должна быть О(1)
+    public TreeMap<TimeOfDay, List<TrainingSession>> getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
+        return timetable.get(dayOfWeek);
     }
 
-    public /* непонятно, что возвращать */ getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
-        //как реализовать, тоже непонятно, но сложность должна быть О(1)
+    public List<TrainingSession> getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek,
+            TimeOfDay timeOfDay) {
+        var daySessions = getTrainingSessionsForDay(dayOfWeek);
+        return daySessions.getOrDefault(timeOfDay, null);
     }
 }
--- test/ru/yandex/practicum/gym/TimetableTest.java
+++ test/ru/yandex/practicum/gym/TimetableTest.java
@@ -3,9 +3,40 @@ package ru.yandex.practicum.gym;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TimetableTest {
+
+    private static TrainingSession getTrainingSessionWithSpecificDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
+        Coach coach = new Coach("Васильев", "Николай", "Сергеевич");
+        Group groupAdult = new Group("Акробатика для взрослых", Age.ADULT, 90);
+        return new TrainingSession(groupAdult, coach, dayOfWeek, timeOfDay);
+    }
+
+    private static Timetable createTimetable() {
+        Timetable timetable = new Timetable();
+
+        Coach coach = new Coach("Васильев", "Николай", "Сергеевич");
+
+        Group groupAdult = new Group("Акробатика для взрослых", Age.ADULT, 90);
+
+        TrainingSession thursdayAdultTrainingSession = new TrainingSession(groupAdult, coach, DayOfWeek.THURSDAY, new TimeOfDay(20, 0));
+
+        timetable.addNewTrainingSession(thursdayAdultTrainingSession);
+
+        Group groupChild = new Group("Акробатика для детей", Age.CHILD, 60);
+        TrainingSession mondayChildTrainingSession = new TrainingSession(groupChild, coach, DayOfWeek.MONDAY, new TimeOfDay(13, 0));
+
+        TrainingSession thursdayChildTrainingSession = new TrainingSession(groupChild, coach, DayOfWeek.THURSDAY, new TimeOfDay(13, 0));
+
+        TrainingSession saturdayChildTrainingSession = new TrainingSession(groupChild, coach, DayOfWeek.SATURDAY, new TimeOfDay(10, 0));
+
+        timetable.addNewTrainingSession(mondayChildTrainingSession);
+        timetable.addNewTrainingSession(thursdayChildTrainingSession);
+        timetable.addNewTrainingSession(saturdayChildTrainingSession);
+        return timetable;
+    }
 
     @Test
     void testGetTrainingSessionsForDaySingleSession() {
@@ -13,57 +44,103 @@ public class TimetableTest {
 
         Group group = new Group("Акробатика для детей", Age.CHILD, 60);
         Coach coach = new Coach("Васильев", "Николай", "Сергеевич");
-        TrainingSession singleTrainingSession = new TrainingSession(group, coach,
-                DayOfWeek.MONDAY, new TimeOfDay(13, 0));
+        TrainingSession singleTrainingSession = new TrainingSession(group, coach, DayOfWeek.MONDAY, new TimeOfDay(13, 0));
 
         timetable.addNewTrainingSession(singleTrainingSession);
 
-        //Проверить, что за понедельник вернулось одно занятие
-        //Проверить, что за вторник не вернулось занятий
+
+        var mondayResults = timetable.getTrainingSessionsForDay(DayOfWeek.MONDAY);
+        var tuesdayResults = timetable.getTrainingSessionsForDay(DayOfWeek.TUESDAY);
+
+        Assertions.assertEquals(1, mondayResults.size());
+        Assertions.assertNull(tuesdayResults);
     }
 
     @Test
     void testGetTrainingSessionsForDayMultipleSessions() {
-        Timetable timetable = new Timetable();
+        Timetable timetable = createTimetable();
 
-        Coach coach = new Coach("Васильев", "Николай", "Сергеевич");
+        var mondaySessions = timetable.getTrainingSessionsForDay(DayOfWeek.MONDAY);
+        var thursdaySessions = timetable.getTrainingSessionsForDay(DayOfWeek.THURSDAY);
+        var tuesdayResults = timetable.getTrainingSessionsForDay(DayOfWeek.TUESDAY);
 
-        Group groupAdult = new Group("Акробатика для взрослых", Age.ADULT, 90);
-        TrainingSession thursdayAdultTrainingSession = new TrainingSession(groupAdult, coach,
-                DayOfWeek.THURSDAY, new TimeOfDay(20, 0));
+        Assertions.assertEquals(1, mondaySessions.size());
+        Assertions.assertEquals(2, thursdaySessions.size());
+        Assertions.assertNull(tuesdayResults);
 
-        timetable.addNewTrainingSession(thursdayAdultTrainingSession);
+        var actualTimes = new ArrayList<>(thursdaySessions.keySet());
 
-        Group groupChild = new Group("Акробатика для детей", Age.CHILD, 60);
-        TrainingSession mondayChildTrainingSession = new TrainingSession(groupChild, coach,
-                DayOfWeek.MONDAY, new TimeOfDay(13, 0));
-        TrainingSession thursdayChildTrainingSession = new TrainingSession(groupChild, coach,
-                DayOfWeek.THURSDAY, new TimeOfDay(13, 0));
-        TrainingSession saturdayChildTrainingSession = new TrainingSession(groupChild, coach,
-                DayOfWeek.SATURDAY, new TimeOfDay(10, 0));
+        List<TimeOfDay> expectedThursdayOrder = new ArrayList<>(List.of(new TimeOfDay(13, 0), new TimeOfDay(20, 0)));
 
-        timetable.addNewTrainingSession(mondayChildTrainingSession);
-        timetable.addNewTrainingSession(thursdayChildTrainingSession);
-        timetable.addNewTrainingSession(saturdayChildTrainingSession);
-
-        // Проверить, что за понедельник вернулось одно занятие
-        // Проверить, что за четверг вернулось два занятия в правильном порядке: сначала в 13:00, потом в 20:00
-        // Проверить, что за вторник не вернулось занятий
+        for (int i = 0; i < expectedThursdayOrder.size(); i++) {
+            var expectedTime = expectedThursdayOrder.get(i);
+            var actualTime = actualTimes.get(i);
+            Assertions.assertEquals(actualTime, expectedTime);
+        }
     }
 
     @Test
-    void testGetTrainingSessionsForDayAndTime() {
+    void testGetTrainingSessionsForDayAndTimePropperAdding() {
         Timetable timetable = new Timetable();
 
         Group group = new Group("Акробатика для детей", Age.CHILD, 60);
         Coach coach = new Coach("Васильев", "Николай", "Сергеевич");
-        TrainingSession singleTrainingSession = new TrainingSession(group, coach,
-                DayOfWeek.MONDAY, new TimeOfDay(13, 0));
+        TrainingSession singleTrainingSession = new TrainingSession(group, coach, DayOfWeek.MONDAY, new TimeOfDay(13, 0));
 
         timetable.addNewTrainingSession(singleTrainingSession);
 
-        //Проверить, что за понедельник в 13:00 вернулось одно занятие
-        //Проверить, что за понедельник в 14:00 не вернулось занятий
+        var mondayOneResults = timetable.getTrainingSessionsForDayAndTime(DayOfWeek.MONDAY, new TimeOfDay(13, 0));
+        Assertions.assertEquals(1, mondayOneResults.size());
+
+        var mondayTwoResults = timetable.getTrainingSessionsForDayAndTime(DayOfWeek.MONDAY, new TimeOfDay(14, 0));
+        Assertions.assertNull(mondayTwoResults);
     }
 
+    @Test
+    void testAddNewTrainingSession() {
+        Timetable timetable = new Timetable();
+
+        DayOfWeek day = DayOfWeek.MONDAY;
+
+        var mondaySessionsBeforeAdding = timetable.getTrainingSessionsForDay(day);
+
+        Assertions.assertNull(mondaySessionsBeforeAdding);
+
+        TrainingSession singleTrainingSession = getTrainingSessionWithSpecificDayAndTime(day, new TimeOfDay(13, 0));
+        timetable.addNewTrainingSession(singleTrainingSession);
+        var mondaySessionsAfterAdding = timetable.getTrainingSessionsForDay(day);
+
+        Assertions.assertEquals(1, mondaySessionsAfterAdding.size());
+    }
+
+    @Test
+    void testSortingLessonsWithOnlyMinutesDiff() {
+        Timetable timetable = new Timetable();
+
+        var timeOdDayOne = new TimeOfDay(13, 30);
+        var timeOdDayTwo = new TimeOfDay(13, 10);
+
+        var weekDay = DayOfWeek.MONDAY;
+
+        List<TimeOfDay> testData = new ArrayList<>(List.of(timeOdDayOne, timeOdDayTwo));
+
+        testData.forEach(i -> {
+            TrainingSession session = getTrainingSessionWithSpecificDayAndTime(weekDay, i);
+            timetable.addNewTrainingSession(session);
+        });
+
+        var sessionsForDay = timetable.getTrainingSessionsForDay(weekDay);
+
+        var actualTimes = new ArrayList<>(sessionsForDay.keySet());
+
+        List<TimeOfDay> sortedTestData = new ArrayList<>(testData);
+
+        sortedTestData.sort(null);
+
+        for (int i = 0; i < sortedTestData.size(); i++) {
+            var expectedTime = sortedTestData.get(i);
+            var actualTime = actualTimes.get(i);
+            Assertions.assertEquals(actualTime, expectedTime);
+        }
+    }
 }
